20303142da38c0ac7a2452fc069caac323049942
objektwerks <objektwerks@runbox.com>
2025-11-06 07:03:32 -0500

    refactored store

--- src/main/scala/lawncare/Store.scala
+++ src/main/scala/lawncare/Store.scala
@@ -34,58 +34,6 @@ final class Store(cache: Cache[String, String],
                   dataSource: DataSource) extends LazyLogging:
   ConnectionPool.singleton(new DataSourceConnectionPool(dataSource))
 
-  def register(account: Account): Account = addAccount(account)
-
-  def login(email: String,
-            pin: String): Option[Account] =
-    DB readOnly { implicit session =>
-      sql"select * from account where email = $email and pin = $pin"
-        .map(rs =>
-          Account(
-            rs.long("id"),
-            rs.string("license"),
-            rs.string("email"),
-            rs.string("pin"),
-            rs.string("activated")
-          )
-        )
-        .single()
-    }
-
-  def isAuthorized(license: String): Boolean =
-    cache.getIfPresent(license) match
-      case Some(_) =>
-        logger.debug(s"*** store cache get: $license")
-        true
-      case None =>
-        val optionalLicense = DB readOnly { implicit session =>
-          sql"select license from account where license = $license"
-            .map(rs => rs.string("license"))
-            .single()
-        }
-        if optionalLicense.isDefined then
-          cache.put(license, license)
-          logger.debug(s"*** store cache put: $license")
-          true
-        else false
-
-  def getAccountEmail(license: String): Option[String] =
-    DB readOnly { implicit session =>
-      sql"select email from account where license = $license"
-        .map(rs => rs.string("email"))
-        .single()
-    }
-
-  def addAccount(account: Account): Account =
-    val id = DB localTx { implicit session =>
-      sql"""
-        insert into account(license, email, pin, activated)
-        values(${account.license}, ${account.email}, ${account.pin}, ${account.activated})
-      """
-      .updateAndReturnGeneratedKey()
-    }
-    account.copy(id = id)
-
   def listProperties(accountId: Long): List[Property] =
     DB readOnly { implicit session =>
       sql"select * from property where account_id = $accountId order by added"
@@ -205,25 +153,4 @@ final class Store(cache: Cache[String, String],
         .map(rs => rs.string("resolved"))
         .single()
         .fold(false)(resolved => resolved != issue.resolved)
-    }
-
-  def listFaults(): List[Fault] =
-    DB readOnly { implicit session =>
-      sql"select * from fault order by occurred desc"
-        .map(rs =>
-          Fault(
-            rs.string("cause"),
-            rs.string("occurred")
-          )
-        )
-        .list()
-    }
-
-  def addFault(fault: Fault): Fault =
-    DB localTx { implicit session =>
-      sql"""
-        insert into fault(cause, occurred) values(${fault.cause}, ${fault.occurred})
-        """
-        .update()
-        fault
     }
