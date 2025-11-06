f2a46c22e7d74a6a96f654b68e5e627be0ad6c9d
objektwerks <objektwerks@runbox.com>
2025-11-06 07:06:03 -0500

    moved and edited ddl.sql

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,14 +1,5 @@
-CREATE TABLE account (
-  id BIGSERIAL PRIMARY KEY,
-  license CHAR(36) UNIQUE NOT NULL,
-  email VARCHAR NOT NULL,
-  pin CHAR(7) NOT NULL,
-  activated VARCHAR(10) NOT NULL
-);
-
 CREATE TABLE property (
   id BIGSERIAL PRIMARY KEY,
-  account_id BIGINT REFERENCES account(id),
   location VARCHAR NOT NULL,
   added VARCHAR(10) NOT NULL
 );
@@ -36,9 +27,4 @@ CREATE TABLE issue (
   resolution VARCHAR NOT NULL,
   reported VARCHAR NOT NULL,
   resolved VARCHAR NOT NULL
-);
-
-CREATE TABLE fault (
-  cause VARCHAR NOT NULL,
-  occurred VARCHAR NOT NULL
 );
